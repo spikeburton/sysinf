a6943a99859b4e44e89b09612a1a9dfd1aea3a78
Spike Burton <sburton@macstadium.com>
2022-06-15 08:15:36 -0400

    add core count

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ import (
 func main() {
 	ver, _ := unix.Sysctl("kern.version")
 	cpu, _ := unix.Sysctl("machdep.cpu.brand_string")
+	cores, _ := unix.SysctlUint32("machdep.cpu.core_count")
 
 	fmt.Printf("System Information\n---\n\n")
 	fmt.Printf("Kernel: \t%s\n", ver)
 	fmt.Printf("CPU: \t\t%s\n", cpu)
+	fmt.Printf("Cores: \t\t%v\n", cores)
 }
